3db13c8f82abe6883b82463598f7dcc2d0cf33ff
naeem khan <naeem78804@gmail.com>
2022-12-19 23:13:50 +0500

    porfolio upload into repo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ========= typing animation ============*/ 
+
+var typed = new typEd(".typing", {
+    Strings:["","Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
